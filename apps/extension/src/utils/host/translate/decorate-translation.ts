d342be27a060b8d8cebcbb2db0c022dbba7fd20d
Holden Hu <sedationh@gmail.com>
2025-08-11 20:35:33 +0800

    fix: improve translation node decoration by adding default style fallback

--- apps/extension/src/utils/host/translate/decorate-translation.ts
+++ apps/extension/src/utils/host/translate/decorate-translation.ts
@@ -3,19 +3,19 @@ import { camelCase } from 'case-anything'
 import { translationNodeStyleSchema } from '@/types/config/provider'
 import { globalConfig } from '@/utils/config/config'
 import { BLOCK_CONTENT_CLASS, INLINE_CONTENT_CLASS, NOTRANSLATE_CLASS } from '@/utils/constants/dom-labels'
-import { CUSTOM_TRANSLATION_NODE_ATTRIBUTE } from '@/utils/constants/translation-node-style'
+import { CUSTOM_TRANSLATION_NODE_ATTRIBUTE, DEFAULT_TRANSLATION_NODE_STYLE } from '@/utils/constants/translation-node-style'
 
 const customTranslationNodeAttribute = camelCase(CUSTOM_TRANSLATION_NODE_ATTRIBUTE)
 
 export function decorateTranslationNode(translatedNode: HTMLElement, translationNodeStyle?: TranslationNodeStyle, nodeType?: 'inline' | 'block') {
-  if (!globalConfig || !translatedNode)
+  if (!translatedNode)
     return
 
   if (nodeType) {
     translatedNode.className = `${NOTRANSLATE_CLASS} ${nodeType === 'inline' ? INLINE_CONTENT_CLASS : BLOCK_CONTENT_CLASS}`
   }
 
-  const customNodeStyle = translationNodeStyle ?? globalConfig.translate.translationNodeStyle
+  const customNodeStyle = translationNodeStyle ?? globalConfig?.translate.translationNodeStyle ?? DEFAULT_TRANSLATION_NODE_STYLE
 
   if (translationNodeStyleSchema.safeParse(customNodeStyle).error)
     return
